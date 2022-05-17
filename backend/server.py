7c8da3dab8db0264f3815cc796b482cca43f74f4
stuartjwright <stuartjwright@gmail.com>
2022-05-17 18:47:40 +0100

    added frontend API calls

--- backend/server.py
+++ backend/server.py
@@ -15,10 +15,10 @@ async def get_new_wall_handler(request):
     try:
         logging.info(request)
         wall = Wall()
-        response_data = wall.get_dict()
-        wall_id = response_data['wall_id']
+        wall_data = wall.get_dict()
+        wall_id = wall_data['wall_id']
         ACTIVE_WALLS[wall_id] = wall
-        return web.json_response(response_data, status=200)
+        return web.json_response(wall_data, status=200)
     except Exception as e:
         logging.error(e)
         return web.json_response({'error': str(e)}, status=500)
@@ -29,12 +29,12 @@ async def receive_guess_handler(request):
     try:
         logging.info(request)
         params = await request.json()
-        wall_id = params['wall_id']
-        guess_ids = params['guess_ids']
+        wall_id = params['wallId']
+        guess_ids = params['guessIds']
         wall = ACTIVE_WALLS[wall_id]
         wall.guess(guess_ids)
-        response_data = wall.get_dict()
-        return web.json_response(response_data, status=201)
+        wall_data = wall.get_dict()
+        return web.json_response(wall_data, status=201)
     except Exception as e:
         logging.error(e)
         return web.json_response({'error': True, 'message': str(e)}, status=500)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,38 @@
+import { useState, useEffect } from "react";
 import "./App.css";
+import { getNewWall, postGuess } from "./requests.js";
 
-function App() {
-  return <div className="App">Hi</div>;
-}
+const App = () => {
+  // Initialise empty wall
+  const [wallData, setWallData] = useState({});
+
+  // Fetch new wall on load
+  useEffect(() => {
+    const fetchWallData = async () => {
+      const newWall = await getNewWall();
+      setWallData(newWall);
+    };
+    fetchWallData();
+  }, []);
+  console.log(wallData);
+
+  // Send user's guess to server, and update wall
+  const sendGuess = async (guessIds) => {
+    const wallId = wallData["wall_id"];
+    const newWall = await postGuess(wallId, guessIds);
+    setWallData(newWall);
+  };
+
+  return (
+    <div
+      className="App"
+      onClick={() => {
+        sendGuess([4, 5, 6, 7]); // placeholder guess
+      }}
+    >
+      Hi
+    </div>
+  );
+};
 
 export default App;
